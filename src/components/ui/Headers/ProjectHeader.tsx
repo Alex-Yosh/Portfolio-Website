0fc35ed2abdc097b1d16a86e2ff15f99ce6845cd
Alex Yoshida <a3yoshid@uwaterloo.ca>
2025-06-08 14:03:27 -0400

    go to projects instead

--- src/components/ui/Headers/ProjectHeader.tsx
+++ src/components/ui/Headers/ProjectHeader.tsx
@@ -20,7 +20,7 @@ export default function ProjectHeader({
         <div className="flex items-center justify-between">
           <div className="flex items-center">
             <Link
-              href="/"
+              href="/#projects"
               className="flex items-center text-blue-400 hover:text-blue-300 transition-colors mr-8"
             >
               <ArrowLeft className="w-5 h-5 mr-2" />
